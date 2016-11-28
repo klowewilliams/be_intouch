0d0d5714f011033738f2929ccfbe5cb83f39f07a
Kimberly Lowe-Williams <kimberly@thedifferenceengine.io>
2016-11-27 22:50:40 -0600

    Added footer, search bar, new route

--- app/controllers/professionals_controller.rb
+++ app/controllers/professionals_controller.rb
@@ -5,5 +5,21 @@ class ProfessionalsController < ApplicationController
 		@professionals = Professional.all
 		@featured = "Takeyah Young"
 	end
+
+	def new
+	end
+
+	def create
+    @professional = Professional.new(name: params[:name], company: params[:company], title: params[:title])
+
+    # Image.create(product_id: @nut.id, image_url: params[:image_1]) if params[:image_1] != ""
+    # Image.create(product_id: @nut.id, image_url: params[:image_2]) if params[:image_2] != ""
+
+    # if @professional.save
+    #   flash[:success] = "You've added a new nut! Welcome to the asylum."
+    #   redirect_to "/professionals/#{professional.id}"
+    # else
+    #   render "/professional/new"
+    end
 	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
 	get "/" => 'professionals#main'
 	get "/beintouch" => 'professionals#main'
+	get "/professionals/new" => 'professionals#new'
+	get "/professionals/:id" => 'professionals#show'
 
 	#get "/user"
 	#get "/profile"
